c819dc76d86962b5e2974f847de041ee3f49d193
Ace <w674901087@163.com>
2017-12-21 23:01:29 +0800

    first test

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
 import cv2
+import os
+
 import calculation
 import zoom
 
-img1 = cv2.imread("Set14/bridge.bmp")
-img2 = cv2.imread("Set14/lenna.bmp")
-img3 = cv2.imread("Set14/pepper.bmp")
-img4 = cv2.imread("Set14/man.bmp")
-img5 = cv2.imread("Set14/baboon.bmp")
-img6 = cv2.imread("Set14/barbara.bmp")
+for filename in os.listdir('HR'):
+    img_hr = cv2.imread('HR/' + filename)
+    print filename + ' loaded from HR/'
 
-# print calculation.PSNR(img2, img3)
-# print calculation.SSIM(img2, img3)
-#
-img_small = zoom.bicubic(img6, 700, 700)
-cv2.imwrite("big6.bmp", img_small)
+    print 'Down-sampling ' + filename + '...'
+    h_lr = img_hr.shape[0] / 3
+    w_lr = img_hr.shape[1] / 3
+    img_lr = zoom.bicubic(img_hr, w_lr, h_lr)
+
+    cv2.imwrite('LR/lr-'+ filename, img_lr)
+    print filename + ' saved as LR/lr-' + filename
+
+    print 'Up-sampling ' + filename + '...'
+    img_bi = zoom.bicubic(img_lr, img_hr.shape[1], img_hr.shape[0])
+    print img_bi.shape
+    cv2.imwrite('BI/bi-'+ filename, img_bi)
+    print filename + ' saved as BI/bi-' + filename
+
+# print calculation.PSNR(img1, img1)
+# print calculation.SSIM(img1, img1)
+
+# bicubic(width, height)
+# img_small = zoom.bicubic(img1, 800, 800)
+# cv2.imwrite("big1.bmp", img_small)
--- zoom.py
+++ zoom.py
@@ -2,15 +2,18 @@ import numpy as np
 import math
 
 # todo grey
-def bicubic(input_img, height, width):
+def bicubic(input_img, width, height):
     (src_height, src_width, depth) = input_img.shape
     in_img = input_img.astype(np.float)
     out_img = np.zeros((height, width, 3));
     ratio_h = float(height) / float(src_height)
     ratio_w = float(width) / float(src_width)
+
+    top, down, left, right, corner = get_padding(in_img, src_height, src_width)
     for i in range(height):
+        if i % 100 == 0:
+            print 'i: ' + str(i) + '/' + str(height - 1)
         for j in range(width):
-            print str(i) + '/' + str(height - 1) + ' ' + str(j) + '/' + str(width - 1)
             x = i / ratio_h
             y = j / ratio_w
             x_int = int(math.floor(x))
@@ -21,9 +24,29 @@ def bicubic(input_img, height, width):
                 for y_diff in range(-1, 3):
                     neib_x = x_int + x_diff
                     neib_y = y_int + y_diff
-                    if neib_x < 0 or neib_x >= src_height or neib_y < 0 or neib_y >= src_width:
-                        continue
-                    out_img[i, j, :] += in_img[neib_x, neib_y, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    # if neib_x < 0 or neib_x >= src_height or neib_y < 0 or neib_y >= src_width:
+                    #     continue
+                    if neib_x < 0:
+                        if neib_y < 0:
+                            out_img[i, j, :] += corner[0, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                        elif neib_y >= src_width:
+                            out_img[i, j, :] += corner[2, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                        else:
+                            out_img[i, j, :] += top[neib_y, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    elif neib_x >= src_height:
+                        if neib_y < 0:
+                            out_img[i, j, :] += corner[1, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                        elif neib_y >= src_width:
+                            out_img[i, j, :] += corner[3, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                        else:
+                            out_img[i, j, :] += down[neib_y, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    elif neib_y < 0:
+                        out_img[i, j, :] += left[neib_x, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    elif neib_y >= src_width:
+                        out_img[i, j, :] += right[neib_x, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    else:
+                        out_img[i, j, :] += in_img[neib_x, neib_y, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
+                    #out_img[i, j, :] += in_img[neib_x, neib_y, :] * get_weight(x - neib_x) * get_weight(y - neib_y)
 
     out_img = np.around(out_img)
     return out_img
@@ -40,20 +63,21 @@ def get_weight(distance):
 
 
 def get_padding(in_img, height, width):
-    top = np.zeros((width, 3));
-    down = np.zeros((width, 3));
-    left = np.zeros((height, 3));
-    right = np.zeros((height, 3));
+    top = np.zeros((width, 3))
+    down = np.zeros((width, 3))
+    left = np.zeros((height, 3))
+    right = np.zeros((height, 3))
 
-    for k in range(height):
+    for k in range(width):
         top[k, :] = 3 * in_img[0, k, :] - 3 * in_img[1, k, :] + in_img[2, k, :]
         down[k, :] = 3 * in_img[height - 1, k, :] - 3 * in_img[height - 2, k, :] + in_img[height - 3, k, :]
+    for k in range(height):
         left[k, :] = 3 * in_img[k, 0, :] - 3 * in_img[k, 1, :] + in_img[k, 2, :]
-        right[k, :] = 3 * in_img[k - width - 1, k, :] - 3 * in_img[k, width - 2, :] + in_img[k, width - 3, :]
+        right[k, :] = 3 * in_img[k, width - 1, :] - 3 * in_img[k, width - 2, :] + in_img[k, width - 3, :]
     corner = np.zeros((4, 3))
     # top_left, down_left, top_right, down_right
     corner[0] = 3 * left[0, :] - 3 * left[1, :] + left[2, :]
     corner[1] = 3 * left[height - 1, :] - 3 * left[height - 2, :] + left[height - 3, :]
     corner[2] = 3 * right[0, :] - 3 * right[1, :] + right[2, :]
-    corner[3] = 3 * right[width - 1, :] - 3 * right[width - 2, :] + right[width - 3, :]
+    corner[3] = 3 * right[height - 1, :] - 3 * right[height - 2, :] + right[height - 3, :]
     return top, down, left, right, corner
